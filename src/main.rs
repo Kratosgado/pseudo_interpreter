6f2d76ccece41c526ff1eb5da84e3c6e5d3db99d
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-03-16 17:10:59 +0000

    initial cli

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 use pseudo_interpreter::{Evaluator, Lexer, Parser};
 
-use  std::fs;
+use std::{env, fs};
 
 fn main() {
-    let filename = "code.ps";
-    let input = fs::read_to_string(filename).expect("Something went to wrong reading file.");
-    let mut lexer = Lexer::new(&input);
-    let tokens = lexer.tokenize();
-    println!("Tokens: {:?}", tokens);
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: pseudo_interpreter <file_path>");
+        std::process::exit(1);
+    }
+    let filename = &args[1];
+    let input = fs::read_to_string(filename);
+    match &input {
+        Ok(inputs) => {
+            let mut lexer = Lexer::new(inputs);
+            let tokens = lexer.tokenize();
+            println!("Tokens: {:?}", tokens);
 
-    let mut parser = Parser::new(tokens);
-    let parsed_token = parser.parse();
-    println!("Parse tree: {:?}", parsed_token);
+            let mut parser = Parser::new(tokens);
+            let parsed_token = parser.parse();
+            println!("Parse tree: {:?}", parsed_token);
 
-    let mut evaluator = Evaluator::new(parsed_token);
-    evaluator.evaluate();
+            let mut evaluator = Evaluator::new(parsed_token);
+            evaluator.evaluate();
+            println!("execution done!");
+            std::process::exit(0);
+        }
+        Err(err) => {
+            eprintln!("{:?}", err);
+            std::process::exit(1);
+        }
+    }
 }
