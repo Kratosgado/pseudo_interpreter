1311e3b7c752e2793848e8d9927b17ee26cac21d
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-03-22 18:06:15 +0000

    add function tests

--- tests/function_test.rs
+++ tests/function_test.rs
@@ -1,9 +1,60 @@
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use pseudo_interpreter::{constants::error_handler::PseudoError, Evaluator, Lexer, Parser};
+    use std::fs;
 
     #[test]
-    fn test_name() {
-        assert_eq!(1, 1);
+    fn function_return()  {
+        let testfile = "tests/test_codes/functions/function.ps";
+        let input = fs::read_to_string(testfile).unwrap();
+        let mut lex = Lexer::new(input.as_str());
+        let tokens = lex.tokenize();
+        assert_eq!(tokens.is_err(), false);
+        let mut parser = Parser::new(tokens.unwrap());
+        let tree = parser.parse();
+        assert_eq!(tree.is_err(), false);
+        let mut eval = Evaluator::new(tree.unwrap());
+        assert_eq!(eval.evaluate().is_err(), false);        
+    }
+    #[test]
+    fn arg_func() {
+        let testfile = "tests/test_codes/functions/arg_func.ps";
+        let input = fs::read_to_string(testfile).unwrap();
+        let mut lex = Lexer::new(input.as_str());
+        let tokens = lex.tokenize();
+        assert_eq!(tokens.is_err(), false);
+        let mut parser = Parser::new(tokens.unwrap());
+        let tree = parser.parse();
+        assert_eq!(tree.is_err(), false);
+        let mut eval = Evaluator::new(tree.unwrap());
+        assert_eq!(eval.evaluate().is_err(), false);
+    }
+
+    #[test]
+    fn no_name_func(){
+        let testfile = "tests/test_codes/functions/noname.ps";
+        let input = fs::read_to_string(testfile).unwrap();
+        let mut lex = Lexer::new(input.as_str());
+        let tokens = lex.tokenize();
+        assert_eq!(tokens.is_err(), false);
+        let mut parser = Parser::new(tokens.unwrap());
+        let tree = parser.parse();
+        assert_eq!(tree.is_err(), true);
+        // let mut eval = Evaluator::new(tree.unwrap());
+        // assert_eq!(eval.evaluate().is_err(), false);
+    }
+
+    #[test]
+    fn unmatched_args()  {
+        let testfile = "tests/test_codes/functions/unmatch_arg.ps";
+        let input = fs::read_to_string(testfile).unwrap();
+        let mut lex = Lexer::new(input.as_str());
+        let tokens = lex.tokenize();
+        assert_eq!(tokens.is_err(), false);
+        let mut parser = Parser::new(tokens.unwrap());
+        let tree = parser.parse();
+        assert_eq!(tree.is_ok(), false);
+        let mut eval = Evaluator::new(tree.unwrap());
+        assert_eq!(eval.evaluate().is_ok(), false);
     }
 }
