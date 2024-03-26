e0356455cf6e5ebbbbdebabef3b0646b973dbc65
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-03-26 19:37:22 +0000

    fix negative stepping

--- src/evaluator/functions/eval_for.rs
+++ src/evaluator/functions/eval_for.rs
@@ -1,16 +1,23 @@
-use crate::{constants::error_handler::PseudoError, evaluator::Expr};
+use crate::{
+    constants::error_handler::PseudoError,
+    evaluator::{eval_result::Operation, Expr},
+};
 
 use super::super::{EvalExpression, EvalResult, EvalStatement, Evaluator, Statement};
 pub trait EvalFor {
-    fn eval_for(&mut self, statement: &Statement ) -> Result<( ), PseudoError>;
+    fn eval_for(&mut self, statement: &Statement) -> Result<(), PseudoError>;
 }
 
 impl EvalFor for Evaluator {
-    fn eval_for(&mut self, statement: &Statement ) -> Result<( ), PseudoError> {
+    fn eval_for(&mut self, statement: &Statement) -> Result<(), PseudoError> {
         if let Statement::For(var, start, end, step, fstatement) = statement {
             let var = match var {
                 Expr::Variable(val) => val.clone(),
-                _ => return Err(PseudoError::VariableError("Expected a variable".to_string())),
+                _ => {
+                    return Err(PseudoError::VariableError(
+                        "Expected a variable".to_string(),
+                    ))
+                }
             };
             let start = match start {
                 Some(start) => self.evaluate_expr(start)?,
@@ -24,18 +31,18 @@ impl EvalFor for Evaluator {
             let step = self.evaluate_expr(step)?;
             self.symbol_table.insert(var.clone(), start);
             while let Some(value) = self.symbol_table.get(&var) {
-                if value <= &end {
+                if value != &end {
                     for statement in fstatement.iter() {
                         self.eval_not_next_statement(statement)?;
                     }
-                    let value = match (self.symbol_table.get(&var).unwrap(), &step) {
-                        (EvalResult::Number(val), EvalResult::Number(step)) => {
-                            EvalResult::Number(val + step)
-                        }
-                        _ => return Err(PseudoError::TypeError("Expected a number".to_string())),
-                    };
+                    let value = self.symbol_table.get(&var).unwrap();
+                    let value = value.add(&step)?;
+
                     self.symbol_table.insert(var.clone(), value);
                 } else {
+                    for statement in fstatement.iter() {
+                        self.eval_not_next_statement(statement)?;
+                    }
                     break;
                 }
             }
